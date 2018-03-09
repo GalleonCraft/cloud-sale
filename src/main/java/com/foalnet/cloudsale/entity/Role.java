5f37a164a49a000f189988317f09843ceb44c94c
GalleonCraft <406525372@qq.com>
2018-03-10 01:11:13 +0800

    实体完善

--- src/main/java/com/foalnet/cloudsale/entity/Role.java
+++ src/main/java/com/foalnet/cloudsale/entity/Role.java
@@ -2,13 +2,17 @@ package com.foalnet.cloudsale.entity;
 
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.Date;
 
 @Table(name = "t_role")
 public class Role {
     @Id
     private Long id;
     private String name;
-    private Integer level;
+    private Date createDate;
+    private Date updateDate;
+    private User createUser;
+    private User updateUser;
 
     public Long getId() {
         return id;
@@ -26,11 +30,35 @@ public class Role {
         this.name = name;
     }
 
-    public Integer getLevel() {
-        return level;
+    public Date getCreateDate() {
+        return createDate;
     }
 
-    public void setLevel(Integer level) {
-        this.level = level;
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public User getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(User createUser) {
+        this.createUser = createUser;
+    }
+
+    public User getUpdateUser() {
+        return updateUser;
+    }
+
+    public void setUpdateUser(User updateUser) {
+        this.updateUser = updateUser;
     }
 }
--- src/main/java/com/foalnet/cloudsale/entity/User.java
+++ src/main/java/com/foalnet/cloudsale/entity/User.java
@@ -2,6 +2,7 @@ package com.foalnet.cloudsale.entity;
 
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.Date;
 
 @Table(name = "t_role")
 public class User {
@@ -10,6 +11,10 @@ public class User {
     private String username;
     private String password;
     private Role role;
+    private Date createDate;
+    private Date updateDate;
+    private User createUser;
+    private User updateUser;
 
     public Long getId() {
         return id;
@@ -41,5 +46,37 @@ public class User {
 
     public void setRole(Role role) {
         this.role = role;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public User getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(User createUser) {
+        this.createUser = createUser;
+    }
+
+    public User getUpdateUser() {
+        return updateUser;
+    }
+
+    public void setUpdateUser(User updateUser) {
+        this.updateUser = updateUser;
     }
 }
